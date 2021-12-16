19eb74f8dd7724067c156c24331407acc4fabe63
AMBIN04527 <siva0035@gmail.com>
2021-12-16 17:49:17 +0530

    springBoot CRUD done

--- src/main/java/com/brainfeaster/boilerPlateV0/controller/homeController.java
+++ src/main/java/com/brainfeaster/boilerPlateV0/controller/homeController.java
@@ -1,10 +1,14 @@
 package com.brainfeaster.boilerPlateV0.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +38,21 @@ public class homeController {
 	@GetMapping("/getById/{id}")
 	public User getUserById(@PathVariable String id) {
 		return userService.getUserId(id);
+	}
+	
+	@GetMapping("/getAll")
+	public List<User> getAllUser() {
+		return userService.getAllUsers();
+	}
+	
+	@PutMapping("/update")
+	public User upadteUser(@RequestBody User user) {
+		return userService.updateUser(user);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteUser(@PathVariable String id) {
+		return userService.deleteUser(id);
 	}
 
 }
--- src/main/java/com/brainfeaster/boilerPlateV0/service/UserService.java
+++ src/main/java/com/brainfeaster/boilerPlateV0/service/UserService.java
@@ -3,6 +3,7 @@ package com.brainfeaster.boilerPlateV0.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import com.brainfeaster.boilerPlateV0.model.User;
 import com.brainfeaster.boilerPlateV0.repository.UserRepository;
 
@@ -18,5 +19,18 @@ public class UserService {
 
 	public User getUserId(String id) {
 		return userRepository.findById(id).get();
+	}
+
+	public List<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+
+	public User updateUser(User user) {
+		return userRepository.save(user);
+	}
+
+	public String deleteUser(String id) {
+		userRepository.deleteById(id);
+		return "User has been deleted";
 	}
 }
